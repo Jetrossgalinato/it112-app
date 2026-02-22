df4ad31442979741685b0f0900c18c820e100e86
Jetross <jetrossneri07@gmail.com>
2026-02-22 19:10:53 +0800

    refactor: change name of the system to JetLog

--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -42,7 +42,7 @@ export function LoginForm({
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Welcome back!</h1>
                 <p className="text-muted-foreground text-balance">
-                  Login to your IT112 account
+                  Login to your JetLog account
                 </p>
               </div>
               <Field>
